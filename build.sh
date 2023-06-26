e9ad9c9de91f0a64cfd15c4b93da15e2bf998030
nic-gaffney <gaffney_nic@protonmail.com>
2023-06-26 00:23:52 -0500

    added  build script

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
+# Build Sloth
 cargo build --features=llvm-sys/prefer-dynamic
+
+# Compile standard library
+./target/debug/sloth std/stdio.sloth
+mv output.o stdio.o
+./target/debug/sloth std/stdlib.sloth
+mv output.o stdlib.io
+./target/debug/sloth std/stdmath.sloth
+mv output.o stdmath.o
+
+# Compile user program
+./target/debug/sloth "$1"
+mv output.o main.o
+
+# Generate binary
+gcc stdio.o std/stdio.c stdlib.o std/stdlib.c stdmath.o std/stdmath.c main.o -o program
